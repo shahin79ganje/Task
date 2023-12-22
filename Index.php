ba9517ca66301bc4d2a1b0bb83d93cb8563cc0ed
shahin79ganje <147970193+shahin79ganje@users.noreply.github.com>
2023-12-22 03:30:09 -0800

    Hello

--- Index.php
+++ Index.php
@@ -3,6 +3,8 @@
 session_start();
 
 // Check if the user is logged in
+// Hello i am shahin
+//Good
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page if not logged in
     header("Location: login.php");
